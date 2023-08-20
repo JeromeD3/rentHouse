14337237d5886357a3f1385e52365260c6bda6c8
邓祥柏 <1760399176@qq.com>
2023-08-20 20:51:16 +0800

    feat: Account页面开发

--- client/src/context/user.tsx
+++ client/src/context/user.tsx
@@ -9,20 +9,24 @@ export const UserContext = createContext({})
 export interface UserContextValue {
   user: string | null
   setUser: (user: string | null) => void
+  ready: boolean
 }
 
 function UserContextProvider({ children }: Component) {
   const [user, setUser] = useState(null)
-
+  const [ready, setReady] = useState(false)
   useEffect(() => {
+    console.log('user context',user);
+     
     if (!user) {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       request.get('/profile').then(({ username }: any) => {
         setUser(username)
+        setReady(true)
       })
     }
   }, [])
-  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
+  return <UserContext.Provider value={{ user, setUser, ready }}>{children}</UserContext.Provider>
 }
 
 export default UserContextProvider
