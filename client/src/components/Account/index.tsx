d5ad861fe7059806b16e7e47df5743c5cac3bf10
邓祥柏 <1760399176@qq.com>
2023-08-23 23:55:54 +0800

    chore: Withdrawal Nav

--- client/src/components/Account/index.tsx
+++ client/src/components/Account/index.tsx
@@ -1,11 +1,9 @@
 import { UserContext, UserContextValue } from '@/context/user'
 import { useContext, useState } from 'react'
-import { Link, Navigate, useParams } from 'react-router-dom'
+import { Navigate, useParams } from 'react-router-dom'
 import request from '@/utils/request'
 import { Place } from '../Place'
-import User from '@/assets/user2.svg'
-import List from '@/assets/list.svg'
-import Building from '@/assets/building.svg'
+import { Nav } from './Nav'
 
 export const Account = () => {
   const { user, ready, setUser } = useContext(UserContext) as UserContextValue
@@ -19,17 +17,6 @@ export const Account = () => {
   if (!ready) return <div>loading...</div>
   if (ready && !user && !redirect) return <Navigate to="/login" />
 
-  function linkClasses(type: string = '') {
-    let classes = 'inline-flex gap-2 px-4 py-2  rounded-full'
-
-    if (type === subpage) {
-      classes += ' bg-primary text-white'
-    } else {
-      classes += ' bg-gray-300'
-    }
-    return classes
-  }
-
   async function logout() {
     await request.post('/logout')
     setRedirect('/')
@@ -39,20 +26,7 @@ export const Account = () => {
   if (redirect) return <Navigate to={redirect} />
   return (
     <div>
-      <nav className="flex w-full mt-8 mb-8 gap-4 justify-center">
-        <Link className={linkClasses('profile')} to={'/account'}>
-          <User />
-          个人中心
-        </Link>
-        <Link className={linkClasses('bookings')} to={'/account/bookings'}>
-          <List />
-          我的预定
-        </Link>
-        <Link className={linkClasses('places')} to={'/account/places'}>
-          <Building />
-          我的房子
-        </Link>
-      </nav>
+      <Nav />
       {subpage === 'profile' && (
         <div className="text-center max-w-lg mx-auto">
           <button onClick={logout} className="primary max-w-sm mt-2">
